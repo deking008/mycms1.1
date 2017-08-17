f0ad4acdf8526a5df26d85728258faffb746cd6b
deking008 <519820597@qq.com>
2017-08-17 23:25:58 +0800

    1231

--- src/controller/common/admin.js
+++ src/controller/common/admin.js
@@ -9,8 +9,19 @@
 module.exports = class extends think.Controller {
 
     async __before() {
-        
+
+        //后台菜单
+        this.adminmenu = await this.model('menu').getallmenu(1,true);
+
+        let notifications ={};
+        notifications.count = 0;
+        notifications.data = [];
+        this.assign({
+            "navxs": false,
+            "bg": "bg-black",
+            "notifications":notifications
+        })
     }
 
-    
+
 }
