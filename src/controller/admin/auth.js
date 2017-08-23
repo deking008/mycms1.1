e423632ee4f21c7ed320e1e89a7a07b4b6faa079
deking008 <519820597@qq.com>
2017-08-23 08:07:51 +0800

    用户组

--- src/controller/admin/auth.js
+++ src/controller/admin/auth.js
@@ -15,6 +15,17 @@ module.exports = class extends Base {
 
     }
     /**
+     * 排序
+     */
+    async sortAction() {
+
+        if(this.para('type')==1){
+            await super.sortAction('member_group','groupid');
+        }else {
+            await super.sortAction('auth_role','id');
+        }
+    }
+    /**
      * 后台节点配置的url作为规则存入auth_rule
      * 执行新节点的插入,已有节点的更新,无效规则的删除三项任务
      * @author
@@ -29,6 +40,23 @@ module.exports = class extends Base {
         let list = await this.model("member_group").order("sort ASC").select();
         this.assign("list",list);
         this.meta_title = "会员组管理";
+        return this.display();
+    }
+    /**
+     * 管理角色管理首页
+     * @returns {*}
+     */
+
+    async adminAction() {
+        let list = await this.model('auth_role').order("id ASC").select();
+        this.assign({
+            "datatables": true,
+            "tactive": "/admin/user",
+            "selfjs": "auth",
+            "list":list
+        })
+        this.active = "admin/auth/index";
+        this.meta_title = "权限管理";
         return this.display();
     }
 }
